50e36ab9a1039bf6eaf5d6efc942c811cc9c63ba
mtzdev <matheus.gol231@gmail.com>
2025-05-09 21:02:52 -0300

    Adicionando testes

--- backend/tests/test_url.py
+++ backend/tests/test_url.py
@@ -1,41 +1,39 @@
+from src.db.models import Link
+
 def test_get_url_not_exists(client):
     response = client.get('/api/short/abcde')
 
     assert response.status_code == 404
     assert response.json()['detail'] == 'Link not found'
 
-def test_get_url_success(client):
-    response = client.post('/api/short', json={'original_url': 'https://www.google.com/', 'short_url': 'abcde'})
+def test_get_url_success(client, simple_url):
+    response = client.get(f'/api/short/{simple_url.short_url}')
 
     assert response.status_code == 200
-    assert response.json()['id'] == 1
-    assert response.json()['original_url'] == 'https://www.google.com/'
-    assert response.json()['short_url'] == 'abcde'
-    assert response.json()['created_at']
+    assert response.json()['original_url'] == simple_url.original_url
 
-    response = client.get('/api/short/abcde')
-    assert response.status_code == 200
-    assert response.json()['original_url'] == 'https://www.google.com/'
-
-def test_get_url_with_password(client):
-    response = client.post('/api/short', json={'original_url': 'https://www.google.com/', 'short_url': 'google', 'password': 'abcde'})
+def test_get_protected_url_with_password(client, protected_url):
+    response = client.get(f'/api/short/{protected_url.short_url}', headers={'password': protected_url.clean_password})
 
     assert response.status_code == 200
-    assert response.json()['id'] == 1
-    assert response.json()['original_url'] == 'https://www.google.com/'
-    assert response.json()['short_url']
-    assert response.json()['created_at']
+    assert response.json()['original_url'] == protected_url.original_url
 
-    response = client.get('/api/short/google')
+def test_get_protected_url_with_invalid_password(client, protected_url):
+    response = client.get(f'/api/short/{protected_url.short_url}', headers={'password': 'wrong_password'})
+
+    assert response.status_code == 401
+    assert response.json()['detail'] == 'Invalid password'
+
+def test_get_protected_url_without_password(client, protected_url):
+    response = client.get(f'/api/short/{protected_url.short_url}')
+
     assert response.status_code == 401
     assert response.json()['detail'] == 'Link is password protected'
-
 
 def test_create_url_simple(client):
     response = client.post('/api/short', json={'original_url': 'https://www.google.com/'})
 
     assert response.status_code == 200
-    assert response.json()['id'] == 1
     assert response.json()['original_url'] == 'https://www.google.com/'
     assert response.json()['short_url']
     assert response.json()['created_at']
@@ -44,7 +42,6 @@ def test_create_url_with_short_url(client):
     response = client.post('/api/short', json={'original_url': 'https://www.google.com/', 'short_url': 'teste'})
 
     assert response.status_code == 200
-    assert response.json()['id'] == 1
     assert response.json()['original_url'] == 'https://www.google.com/'
     assert response.json()['short_url'] == 'teste'
     assert response.json()['created_at']
@@ -55,14 +52,20 @@ def test_create_url_with_invalid_short_url(client):
     assert response.status_code == 400
     assert response.json()['detail'] == 'Invalid short URL'
 
-def test_create_url_with_existing_short_url(client):
-    response1 = client.post('/api/short', json={'original_url': 'https://www.google.com/', 'short_url': 'google'})
+def test_create_url_with_existing_short_url(client, simple_url):
+    short_url = simple_url.short_url
 
-    assert response1.status_code == 200
-    assert response1.json()['id'] == 1
-    assert response1.json()['original_url'] == 'https://www.google.com/'
-    assert response1.json()['short_url'] == 'google'
+    response = client.post('/api/short', json={'original_url': 'https://www.google.com.br/', 'short_url': short_url})
+    assert response.status_code == 400
+    assert response.json()['detail'] == 'Short URL already exists'
 
-    response2 = client.post('/api/short', json={'original_url': 'https://www.google.com.br/', 'short_url': 'google'})
-    assert response2.status_code == 400
-    assert response2.json()['detail'] == 'Short URL already exists'
+def test_create_url_with_password(client, db_session):
+    response = client.post('/api/short', json={'original_url': 'https://www.google.com.br/', 'password': '123456'})
+
+    assert response.status_code == 200
+    assert response.json()['original_url'] == 'https://www.google.com.br/'
+
+    link = db_session.query(Link).filter(Link.short_url == response.json()['short_url']).first()
+
+    assert link.password is not None
+
